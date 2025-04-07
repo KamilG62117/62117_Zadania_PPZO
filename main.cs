acc55ccd25545dcba951c2e41931e217ef536505
KamilG62117 <kamil.grzywinski@student.wsb.edu.pl>
2025-04-07 12:08:29 +0000

    Restored to '717496fd23fdc2798b8f3a7124562451c481f005'
    Replit-Restored-To: 717496fd23fdc2798b8f3a7124562451c481f005

--- main.cs
+++ main.cs
@@ -1,41 +1,35 @@
-
 using System;
 
 class Program {
   public static void Main (string[] args) {
     int wybor;
     double stopnie;
-    do {
+    do{
       Console.WriteLine("Wybierz rodzaj konwersji:");
       Console.WriteLine("[0]Zamknij");
-      Console.WriteLine("[1]Ze stopni Celsjusza na Fahrenheita");
-      Console.WriteLine("[2]Ze stopni Fahrenheita na Celsjusza");
-      
+      Console.WriteLine("[1]Ze stopni Celsjusza na Fahrenheita"");
+      Console.WriteLine("[2]Ze stopni Fahrenheita na Celsjusza"");
+      do{
       Console.Write("Wybór: ");
-      wybor = int.Parse(Console.ReadLine());
-      
-      if (wybor == 1) {
+      wybor = int.Parse(Console.ReadLine())
+      if (wybor == 1){
         Console.WriteLine("Podaj stopnie Celsjusza:");
         stopnie = double.Parse(Console.ReadLine());
         Console.WriteLine("Stopnie Fahrenheita: " + (stopnie * 1.8 + 32));
       }
-      else if (wybor == 2) {
+      else if (wybor == 2){
         Console.WriteLine("Podaj stopnie Fahrenheita:");
         stopnie = double.Parse(Console.ReadLine());
-        Console.WriteLine("Stopnie Celsjusza: " + ((stopnie - 32) / 1.8));
+        Console.WriteLine("Stopnie Celsjusza: " + (stopnie - 32) / 1.8)
       }
-      else if (wybor != 0) {
-        Console.WriteLine("Zły wybór. Spróbuj ponownie.");
-      }
-
-      if (wybor != 0) {
-        Console.WriteLine("");
-        Console.WriteLine("Naciśnij Enter, aby wykonać następną konwersje...");
-        Console.ReadLine();
-        Console.Clear();
-      }
-    } while (wybor != 0);
-    
-    Console.WriteLine("Zamykanie programu...");
+      else{
+        Console.WriteLine("Zły wybór. Spróbuj ponownie.")
+      }}while (wybor !=1 && wybor !=2);
+      Console.WriteLine("");
+      Console.WriteLine("Naciśnij Enter, aby wykonać następną konwersje...");
+      Console.ReadLine();
+      Console.Clear();
+      } while (wybor != 0);
+     Console.WriteLine("Zamykanie programu...");
   }
 }
